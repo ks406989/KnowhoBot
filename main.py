b7c7492549c766faa7d886d1cb7242c56f947d58
ks406989 <60415968+ks406989@users.noreply.github.com>
2020-10-05 12:26:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,13 +16,13 @@ app = Client(
 
 @app.on_message(filters.command(["start"]))
 def start(client, message):
-    client.send_message(chat_id=message.chat.id,text=f"`Hi` **{message.from_user.first_name}**\n `Enter the number to search...`")
+    client.send_message(chat_id=message.chat.id,text=f"`Hi` **{message.from_user.first_name}**\n `Enter the number to search... \n Join Our Channel For Latest Movies \n @HindiXstream`")
     check_status = check(message.chat.id)
 
 
 @app.on_message(filters.command(["about"]))
 def about(client, message):
-    client.send_message(chat_id=message.chat.id, reply_to_message_id=message.message_id,text=f"`Bot`            : [knowhobot](t.me/knowhobot)\n`Creator :` [agentnova](t.me/agentnova)\n`Language:` [Python3](https://python.org)\n`Library :` [Pyrogram](https://docs.pyrogram.org/) \n`Server  :` [Heroku](https://herokuapp.com/)",disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Give Feedback", url="t.me/agentnova")]]))
+    client.send_message(chat_id=message.chat.id, reply_to_message_id=message.message_id,text=f"`Channel`            : [Hindi Latest Movie's](t.me/)\n`Creator :` [Kamlesh](t.me/Kamlesh_Kirodewal)\n`Language:` [Python3](https://python.org)\n`Library :` [Pyrogram](https://docs.pyrogram.org/) \n`Server  :` [Heroku](https://herokuapp.com/)",disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Give Feedback", url="t.me/Kamlesh_Kirodewal")]]))
 
 
 @app.on_message(filters.command(["log"]))

4cad84cb64d96022d44354eb01eeb035c2047b16
ks406989 <60415968+ks406989@users.noreply.github.com>
2020-10-05 14:15:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def start(client, message):
 
 @app.on_message(filters.command(["about"]))
 def about(client, message):
-    client.send_message(chat_id=message.chat.id, reply_to_message_id=message.message_id,text=f"`Channel`            : [Movies](t.me/hindixstream)\n`Creator :` [Kamlesh](t.me/Kamlesh_Kirodewal)\n`Language:` [Python3](https://python.org)\n`Library :` [Pyrogram](https://docs.pyrogram.org/) \n`Server  :` [Heroku](https://herokuapp.com/)",disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Give Feedback", url="t.me/Kamlesh_Kirodewal")]]))
+    client.send_message(chat_id=message.chat.id, reply_to_message_id=message.message_id,text=f"`Channel`: [Movies](t.me/hindixstream)\n`Creator :` [Kamlesh Kirodewal](t.me/Kamlesh_Kirodewal)\n`Language:` [Python3](https://python.org)\n`Library :` [Pyrogram](https://docs.pyrogram.org/) \n`Server  :` [Heroku](https://herokuapp.com/)",disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Give Feedback", url="t.me/Kamlesh_Kirodewal")]]))
 
 
 @app.on_message(filters.command(["log"]))
